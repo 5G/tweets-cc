ac170b11ec4d89b37a2c88014af58033209cfdca
5G <james@giveback.ie>
2020-08-31 14:42:26 +0100

    Added react-flip-move

--- src/Feed.js
+++ src/Feed.js
@@ -3,6 +3,7 @@ import "./Feed.css";
 import TweetBox from "./TweetBox";
 import Post from "./Post";
 import db from "./firebase";
+import FlipMove from "react-flip-move";
 
 function Feed() {
   const [posts, setPosts] = useState([]);
@@ -19,24 +20,29 @@ function Feed() {
       </div>
       <TweetBox />
 
-      {posts.map((post) => (
+      <FlipMove>
+        {posts.map((post) => (
+          <Post
+            key={
+              post.text
+            } /* Update to use Firebase documentID in production */
+            displayName={post.displayName}
+            username={post.username}
+            verified={post.verified}
+            text={post.text}
+            avatar={post.avatar}
+            image={post.image}
+          />
+        ))}
+        {/* Default post in case of firebase error */}
         <Post
-          displayName={post.displayName}
-          username={post.username}
-          verified={post.verified}
-          text={post.text}
-          avatar={post.avatar}
-          image={post.image}
+          displayName="John Doe"
+          username="jdoe"
+          verified
+          text="Lorem ipsum otter sit"
+          image="https://media2.giphy.com/media/QcIhKTnPVZu48/giphy.gif"
         />
-      ))}
-
-      <Post
-        displayName="John Doe"
-        username="jdoe"
-        verified
-        text="Lorem ipsum otter sit"
-        image="https://media2.giphy.com/media/QcIhKTnPVZu48/giphy.gif"
-      />
+      </FlipMove>
     </div>
   );
 }
